ed6387052dd3ba6729d5edd5d6ea47d27d2c401f
maneishbisht <maneishbisht7342@gmail.com>
2026-06-06 07:13:34 +0530

    REDIS_CONFIG_MISMATCH_FIX

--- Server/config/redis.js
+++ Server/config/redis.js
@@ -1,8 +1,6 @@
 import Redis from 'ioredis';
 
-const redis = new Redis({
-  host: (process.env.REDIS_HOST)||'127.0.0.1',
-  port: parseInt((process.env.REDIS_PORT) || '6379'),
+const redis = new Redis(process.env.REDIS_HOST || 'redis://127.0.0.1:6379', {
   maxRetriesPerRequest: null,
   retryStrategy(times) {
     if (times > 3) return null;
